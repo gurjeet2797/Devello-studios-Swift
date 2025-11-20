f9d038acdbaefc37c402c7e7b02eb9b5d58d54da
Gurjeet Singh <gurjeet2797@gmail.com>
2025-11-19 20:32:41 -0500

    Initial Commit

---
+++ Devello Studios/Devello_StudiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Devello_StudiosApp.swift
+//  Devello Studios
+//
+//  Created by Gurjeet Singh on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Devello_StudiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
